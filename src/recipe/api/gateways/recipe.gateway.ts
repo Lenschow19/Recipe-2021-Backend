db75f3a9aa488273715f3742612815d7f63d953a
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2021-04-26 15:27:59 +0200

    Update recipe.gateway.ts

--- src/recipe/api/gateways/recipe.gateway.ts
+++ src/recipe/api/gateways/recipe.gateway.ts
@@ -1,15 +1,12 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Inject, UseGuards } from '@nestjs/common';
+import { Inject } from '@nestjs/common';
 import { IRecipeService, IRecipeServiceProvider } from '../../core/primary-ports/recipe.service.interface';
 import { Recipe } from '../../core/models/recipe';
 import { ListenDetailsDto } from '../dtos/listen.details.dto';
 import { Socket } from 'socket.io';
-import { JwtAuthGuard } from '../../../auth/jwt-auth.guard';
 
 @WebSocketGateway()
 export class RecipeGateway {
-
-  recipeDetailsRoom: string = 'details'
 
   @WebSocketServer() server;
 
@@ -17,15 +14,17 @@ export class RecipeGateway {
 
   @SubscribeMessage('updateRecipe')
   async handleUpdateEvent(@MessageBody() recipe: Recipe) {
-
-    //Emit to details for Recipe
-    this.server.in(`${this.recipeDetailsRoom}${recipe.ID}`).emit('recipeUpdated', recipe);
-
+    this.server.emit('recipeUpdated', recipe);
   }
 
-  @SubscribeMessage('joinDetailsRoom')
-  async joinDetailsRoom(@MessageBody() listenDetailsDTO: ListenDetailsDto, @ConnectedSocket() client: Socket) {
-    client.leaveAll();
-    client.join(`${listenDetailsDTO.room}${listenDetailsDTO.ID}`);
+  @SubscribeMessage('createRecipe')
+  async handleCreateEvent(@MessageBody() recipe: Recipe) {
+    this.server.emit('recipeCreated', recipe);
   }
+
+  // @SubscribeMessage('joinDetailsRoom')
+  // async joinDetailsRoom(@MessageBody() listenDetailsDTO: ListenDetailsDto, @ConnectedSocket() client: Socket) {
+  //   client.leaveAll();
+  //   client.join(`${listenDetailsDTO.room}${listenDetailsDTO.ID}`);
+  // }
 }
